4825b31be2c4caba9ab0616dcdfd6c20cb3ca083
DarkPurpleShadow <hosen.sind.tot@gmail.com>
2016-04-07 14:46:44 +0200

    Added protagonist and began to properly implement play_game

---
+++ protagonist.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3.4
+
+from character import Character
+
+
+class Protagonist(Character):
+
+    def set_level(self, new_level):
+        self.level = new_level
+
+    def get_level(self):
+        return self.level
+
+    def increase_level(self):
+        self.level += 1
+
+    def set_location(self, new_location):
+        self.location = new_location
+
+    def get_location(self):
+        return self.location
+
+    def get_rucksack(self):
+        return self.rucksack
+
+    def add_to_rucksack(self, obj):
+        self.rucksack.append(obj)
+
+    def is_weapon_equipped(self):
+        return self.weaponInHand is not None
+
+    def is_shield_equipped(self):
+        return self.shieldInHand is not None
+
+    def __init__(self, name, description):
+        Character.__init__(self, name, description, True)
+        self.level = 1
+        self.location = 0
+        self.rucksack = []
+        self.weaponInHand = None
+        self.shieldInHand = None
--- role_game.py
+++ role_game.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3.4
 
 from game_reader import GameReader
-from place import Place
+# from place import Place
+from protagonist import Protagonist
+GAME_COMMANDS = ["LOOK", "SEARCH", "GO", "FIGHT","EXIT", "PICKUP"];
 
 
 class Game:
 
-    # Play!
     @staticmethod
-    def play_game():
+    def __print_commands():
+        for s in sorted(GAME_COMMANDS):
+            print(s)
+
+    # Play!
+    def play_game(self):
         # TODO: code
-        string = "play_game to be coded"
-        print(string.upper())
+        print("What do you wish to do?")
+        self.__print_commands()
 
         split_command = ["BEGIN"]
         while split_command[0] != "EXIT":
             command = input()
             split_command = command.split()
             split_command[0] = split_command[0].upper()
-            if split_command[0] == "GO":
+
+            if split_command[0] == "LOOK":
+                if len(split_command) == 1:
+                    # todo: print current location
+                    pass
+                else:
+                    # todo: print desired stuff
+                    pass
+            elif split_command[0] == "GO":
                 print("going to some place")
             elif split_command[0] == "FIGHT":
                 print("fighting")
             elif split_command[0] == "EXIT":
                 print("bye")
+            else:
+                self.__print_commands()
 
     # Loads a game
     def load(self, path):
@@ -32,7 +48,6 @@ class Game:
         gr = GameReader()
         gr.parse_game(path)
 
-        # TODO:
         # 1 - places fertig einrichten (liste mit places geben, bisher haben wir nur id's)
         # We need the places to be in order
         self.places_array = sorted(gr.get_places_list(), key=lambda place: place._id)
@@ -47,27 +62,30 @@ class Game:
         self.enemies_array = gr.get_enemies_list()
 
         # 3 - temporaer protagonist erzeugen. In der zukunft soll es auch gelesen werden
+        self.protagonist = Protagonist("Cris", "Cris is a demo warrior")
 
     # Returns true for loading a game, or false for exiting
-    # TODO: insert a while
     def load_or_exit(self):
-        print("load games/DemoGame.xml")
-        user_command = input("Type \"load $GAME_PATH\" for loading a new game.\nType \"exit\" to exit program:\n")
-        split_command = user_command.split()
-        if split_command[0].lower() == "load":
-            if len(split_command) == 2:
-                self.load(split_command[1])
-                return True
-            else:
-                print("Error occured: syntax must be \"load $GAME_DIR\"")
-                return False
-        else:
-            return False
+        print("load games/DemoGame.xml") # for testing purposes
+
+        split_command = ["0"]
+        while split_command[0] != "EXIT":
+            user_command = input("Type \"load $GAME_PATH\" for loading a new game.\nType \"exit\" to exit program:\n")
+            split_command = user_command.split()
+            split_command[0] = split_command[0].upper()
+            if split_command[0] == "LOAD":
+                if len(split_command) == 2:
+                    self.load(split_command[1])
+                    return True
+                else:
+                    print("Error occurred: syntax must be \"load $GAME_DIR\"")
+        return False
 
     def __init__(self):
         self.places_array = None
         self.begin_place = None
         self.enemies_array = None
+        self.protagonist = None
 
 
 if __name__ == '__main__':
