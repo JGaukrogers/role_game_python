ed33caaad4c874c2a0ae150081c130480579cc72
DarkPurpleShadow <hosen.sind.tot@gmail.com>
2016-04-14 20:36:32 +0200

    Enhanced see statistics

--- app/src/main/java/cat/judith/stopsmoking/SeeStatistics.java
+++ app/src/main/java/cat/judith/stopsmoking/SeeStatistics.java
@@ -6,26 +6,44 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SeeStatistics extends AppCompatActivity {
 
-    private TextView cigsSmokedView;
-    private TextView moneySmokedView;
-
     //TODO: get from settings
-    private double price = 5;
-    private int cigsPerPacket = 20;
+    private static final double price = 5;
+    private static final int cigsPerPacket = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_statistics);
 
-        cigsSmokedView = (TextView) findViewById(R.id.cigsSmokedView);
-        moneySmokedView = (TextView) findViewById(R.id.monexSmokedView);
+        TextView cigsSmokedView = (TextView) findViewById(R.id.cigsSmokedView);
+        TextView moneySmokedView = (TextView) findViewById(R.id.moneySmokedView);
+        TextView averageSmokedView = (TextView) findViewById(R.id.avgSmokedView);
 
         int totalCigsSmoked = getTotalCigarettesSmoked();
         cigsSmokedView.setText(totalCigsSmoked + " cigarettes smoked");
         moneySmokedView.setText(getTotalMoneySmoked(totalCigsSmoked) + " € smoked");
+        averageSmokedView.setText(getAverageCigarettesSmoked(totalCigsSmoked) + " cigarettes/day smoked");
+
+        // Init. and set graph
+        GraphView graph = (GraphView) findViewById(R.id.graphView);
+        BarGraphSeries<DataPoint> graphCigsDay = getCigsPerDayPoints();
+
+        graph.addSeries(graphCigsDay);
+
+        java.text.DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext(), dateFormat));
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Cigarettes Smoked");
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
 
     }
 
@@ -41,18 +59,80 @@ public class SeeStatistics extends AppCompatActivity {
             sum_cigs_smoked = c.getInt(0);
         else
             sum_cigs_smoked = -1;
+        c.close();
         db.close();
 
         return sum_cigs_smoked;
     }
 
     private double getTotalMoneySmoked(int totalCigarettes) {
-        double totalMoney = getPriceForSingleCig() * totalCigarettes;
-        return totalMoney;
+        return getPriceForSingleCig() * totalCigarettes;
     }
 
     private double getPriceForSingleCig() {
         return price / cigsPerPacket;
+    }
+
+    private int getSumDaysSmoked(){
+
+        int sum_days_smoked;
+
+        CigarettesSmokedDB dbHelper = new CigarettesSmokedDB(this.getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery(CigarettesSmokedDB.SQL_GET_DAYS_SMOKED, null);
+        if(c.moveToFirst())
+            sum_days_smoked = c.getInt(0);
+        else
+            sum_days_smoked = -1;
+        db.close();
+
+        return sum_days_smoked;
+    }
+
+    private float getAverageCigarettesSmoked(int totalCigarettes){
+
+        float daysSmoked = getSumDaysSmoked();
+        float avg_cigs_smoked = 0;
+
+        if(daysSmoked != 0){
+            avg_cigs_smoked = totalCigarettes / daysSmoked;
+        }
+        return avg_cigs_smoked;
+    }
+
+    private BarGraphSeries<DataPoint> getCigsPerDayPoints() {
+
+        CigarettesSmokedDB dbHelper = new CigarettesSmokedDB(this.getApplicationContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor c = db.rawQuery("SELECT * FROM " + CigarettesSmokedDB.TABLE_NAME + " ORDER BY " + CigarettesSmokedDB.COLUMN_NAME_ENTRY_ID + " ASC ", null);
+
+        c.moveToFirst();
+        DataPoint dataPointArray[] = new DataPoint[c.getCount()];
+
+        for (int i = 0; i < c.getCount(); i++){
+            DataPoint dpAux = new DataPoint(convertToDate(c.getInt(1)), c.getInt(2));
+            dataPointArray[i] = dpAux;
+            c.moveToNext();
+        }
+
+        BarGraphSeries<DataPoint> graphCigsDay = new BarGraphSeries<>(dataPointArray);
+        graphCigsDay.setSpacing(20);
+
+        return graphCigsDay;
+    }
+
+    private Date convertToDate(int iDate){
+
+
+        int year = (iDate / 10000) - 1900;
+        int month = ((iDate / 100) % 100) - 1;
+        int day = iDate % 100;
+        Date date = new Date(year, month, day);
+
+        return date;
+
     }
 
 }
