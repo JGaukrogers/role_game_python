cfff0e5f11c23f02a4230e80d254ae674724d024
DarkPurpleShadow <hosen.sind.tot@gmail.com>
2016-04-15 18:49:24 +0200

    Style edit

--- app/src/main/java/cat/judith/stopsmoking/CigaretteCounter.java
+++ app/src/main/java/cat/judith/stopsmoking/CigaretteCounter.java
@@ -1,7 +1,6 @@
 package cat.judith.stopsmoking;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
